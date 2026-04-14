dedc1c17dc029007e153731cd7ad569c1017c581
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2026-04-14 18:48:45 +0200

    login form wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginFormComponent } from '../app/login-form/login-form.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'authentificationTP';
+
 }
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Connexion avec :', this.loginForm.value);
+    }
+  }
+
+  onCancel(): void {
+    this.loginForm.reset();
+  }
 }
